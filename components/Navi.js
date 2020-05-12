b987c2b518ab8fd137dcb4e7ef13fb24fc87e2a4
Alejandro Segura <eloso85@gmail.com>
2020-05-12 10:54:52 -0500

    change links

--- components/Navi.js
+++ components/Navi.js
@@ -12,7 +12,7 @@ export default function Navi(){
   <Navbar >
     
     <Nav className="mr-auto" >
-      <Nav.Link href="#home">
+      <Nav.Link href="/main">
       <svg  width="2em" height="2em" viewBox="0 0 16 16" fill="#0056B3" xmlns="http://www.w3.org/2000/svg">
   <path  d="M8 3.293l6 6V13.5a1.5 1.5 0 01-1.5 1.5h-9A1.5 1.5 0 012 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5z" />
   <path  d="M7.293 1.5a1 1 0 011.414 0l6.647 6.646a.5.5 0 01-.708.708L8 2.207 1.354 8.854a.5.5 0 11-.708-.708L7.293 1.5z" />Home
@@ -22,13 +22,13 @@ export default function Navi(){
      
 
 
-      <Nav.Link href="#features">
+      <Nav.Link href="/contact">
 
       <svg  width="2em" height="2em" viewBox="0 0 16 16" fill="#0056B3" xmlns="http://www.w3.org/2000/svg">
   <path d="M.05 3.555L8 8.414l7.95-4.859A2 2 0 0014 2H2A2 2 0 00.05 3.555zM16 4.697l-5.875 3.59L16 11.743V4.697zm-.168 8.108L9.157 8.879 8 9.586l-1.157-.707-6.675 3.926A2 2 0 002 14h12a2 2 0 001.832-1.195zM0 11.743l5.875-3.456L0 4.697v7.046z"/>
 </svg>
       </Nav.Link>
-      <Nav.Link href="#pricing">
+      <Nav.Link href="/projects">
 
       <svg  width="2em" height="2em" viewBox="0 0 16 16" fill="#0056B3" xmlns="http://www.w3.org/2000/svg">
   <path  d="M0 12.5A1.5 1.5 0 001.5 14h13a1.5 1.5 0 001.5-1.5V6.85L8.129 8.947a.5.5 0 01-.258 0L0 6.85v5.65z" />
