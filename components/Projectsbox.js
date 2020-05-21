127f19e5105845871591e26a432591dbb38976c1
Alejandro <eloso85@gmail.com>
2020-05-21 17:28:14 -0500

    working on projects page

--- components/Projectsbox.js
+++ components/Projectsbox.js
@@ -1,37 +1,72 @@
-import unime from '../src/images/unicorn_me.jpg'
+import rescipe from '../src/images/recipe.jpg'
 
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Image from 'react-bootstrap/Image'
 
 //hello
 export default function Projectsbox() {
 
     return (<>
-        <div className="container">
+        <div className="container1">
             <Container>
                 <Row>
                     <Col>
 
                         <Card>
                             <Card.Header>
-                                <Jumbotron fluid>
-                                    <Container style={{textAlign:'center'}}>
-                                        <h1>Fluid jumbotron</h1>
+                                <Jumbotron fluid className='jumbo' style={{ textAlign: 'center' }}>
+                                    
+                                    
+                                        <h1>Projects</h1>
                                         <p>
                                             This is a modified jumbotron that occupies the entire horizontal space of
                                             its parent.
                                         </p>
-                                    </Container>
+                                    
                                 </Jumbotron>
                             </Card.Header>
                             <Card.Body>
-                                <Card.Title>My Projects</Card.Title>
-                                <Card.Text>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et sollicitudin augue, eget mattis neque. Nullam molestie, purus sed vulputate fermentum, lacus dui volutpat lorem, sed bibendum ante ex non nunc. Fusce nec felis vitae turpis tincidunt iaculis cursus vitae mi. Nunc ultrices eget turpis eu rutrum. Integer ullamcorper orci nec velit lacinia, nec rhoncus libero lobortis. Etiam varius molestie tellus nec egestas. Donec mauris orci, vestibulum quis semper id, volutpat at lectus. Integer justo lacus, sagittis eget orci sit amet, ullamcorper ullamcorper purus. Quisque sit amet nunc sed tortor mollis convallis porttitor vel risus. Integer arcu magna, placerat ornare ultrices at, faucibus vitae nunc. Sed a dui metus. Sed eu nisl vel velit varius dignissim a sit amet libero. Nunc in eros sollicitudin, varius lacus vel, pellentesque risus. Quisque eu commodo risus, et ultricies ex.
-                                </Card.Text>
+                                <Card.Title>Wat2Eat</Card.Title>
+
+                                <Container>
+                                    <Row>
+                                        <Col xs={9} >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan metus sit amet tincidunt auctor. Vestibulum in odio dui. Ut non ex arcu. In nec consectetur augue, ut euismod justo. Maecenas ornare lacus eget tortor placerat blandit. Vivamus condimentum, ex accumsan facilisis dignissim, quam metus dignissim odio, sed porttitor nibh ex ut orci. Nulla nisl nisi, sagittis nec augue non, vulputate lobortis purus.</Col>
+                                        <Col >
+                                            <Image style={{ width: '150px' }} src={rescipe} rounded />
+                                        </Col>
+
+                                    </Row>
+
+                                    <Row>
+
+
+
+
+                                        <Col md="auto" >
+                                            <Image style={{ width: '150px' }} src={rescipe} rounded />
+                                        </Col>
+                                        <Col>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan metus sit amet tincidunt auctor. Vestibulum in odio dui. Ut non ex arcu. In nec consectetur augue, ut euismod justo. Maecenas ornare lacus eget tortor placerat blandit. Vivamus condimentum, ex accumsan facilisis dignissim, quam metus dignissim odio, sed porttitor nibh ex ut orci. Nulla nisl nisi, sagittis nec augue non, vulputate lobortis purus.</Col>
+
+
+                                    </Row>
+
+                                    <Row>
+                                        <Col xs={9} >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan metus sit amet tincidunt auctor. Vestibulum in odio dui. Ut non ex arcu. In nec consectetur augue, ut euismod justo. Maecenas ornare lacus eget tortor placerat blandit. Vivamus condimentum, ex accumsan facilisis dignissim, quam metus dignissim odio, sed porttitor nibh ex ut orci. Nulla nisl nisi, sagittis nec augue non, vulputate lobortis purus.</Col>
+                                        <Col >
+                                            <Image style={{ width: '150px' }} src={rescipe} rounded />
+                                        </Col>
+
+                                    </Row>
+                                </Container>
+
+
+
+
+
 
 
                             </Card.Body>
@@ -42,15 +77,21 @@ export default function Projectsbox() {
             </Container>
         </div>
 
-        <style jsx>{`
+        <style jsx global >{`
     
-    .container{
+    .container1{
         
-        background-color:white;
+        //background-color:white;
           color:black;
-          border:solid;
+          //border:solid;
           padding:1%;
     }
+    
+    .jumbo{
+        background-image:url(${rescipe});
+        background-size:cover;
+    }
+    
     
     
     
