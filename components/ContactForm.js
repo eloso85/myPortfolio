21909cbe49b90bcc1012a29793efc399f6016744
Alejandro <eloso85@gmail.com>
2020-05-22 13:18:05 -0500

    env

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -18,6 +18,24 @@ export default function ContactForm() {
     const [disabled, setDisabled] = useState(false)
     const [emailSent, setEmailSent] = useState(null)
 
+    const handleSubmit =(evt)=>{
+        evt.preventDefault();
+
+       
+
+        // setDisabled({
+        //     disabled:true
+        //     });
+
+        // setEmailSent({
+        //     emailSent:false,
+    
+            
+        // })    
+        // alert("email not sent")
+        // console.log(evt)
+    }
+
 
     return (<>
 
@@ -35,7 +53,7 @@ export default function ContactForm() {
                             </Jumbotron>
                         </Card.Header>
                         <Card.Body>
-                            <Form>
+                            <Form onSubmit={handleSubmit}>
                                 <Form.Group controlId="exampleForm.ControlInput1">
                                     <Form.Label>Name</Form.Label>
                                     <Form.Control type="name" placeholder="John Smith" value={name}  onChange={e=>setName(e.target.value)} />
@@ -45,14 +63,19 @@ export default function ContactForm() {
                                     <Form.Label>Email address</Form.Label>
                                     <Form.Control type="email" placeholder="name@example.com"  value={email} onChange={e=>setEmail(e.target.value)}/>
                                 </Form.Group>
-                            </Form>
+                            
                             <Form.Group controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Message</Form.Label>
-                                <Form.Control as="textarea" rows="3"  onChange={()=>setMessage(message)} />
+                                <Form.Control as="textarea" rows="3"  value ={message} onChange={e=>setMessage(e.target.value)} />
                             </Form.Group>
-                            <Button variant="primary"  active>
+                            <Button variant="primary" type="submit" disabled= {disabled}>
                                 Send
-                            </Button>{' '}
+                            </Button>
+
+                            {emailSent === true && <p className="success-msg">Email Sent</p>}
+                            {emailSent === false && <p className="err-msg">Email Not Sent</p>}
+
+                        </Form>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -73,6 +96,16 @@ export default function ContactForm() {
           color:black;
           border:solid;
           padding:1%;
+    }
+
+    .success-msg{
+        color: #57a773;
+        padding: 10px 15px;
+    }
+
+    .err-msg{
+        color: red;
+        padding: 10px 15px;
     }
     
     
