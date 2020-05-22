821f5b08af6556c62e7fa8fc9ebe81f14dddb5d3
Alejandro <eloso85@gmail.com>
2020-05-21 23:29:04 -0500

    making value work

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,3 +1,5 @@
+import React, {useState}  from 'react'
+
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
@@ -6,9 +8,17 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-import email from '../src/images/email2.jpg'
+import email1 from '../src/images/email2.jpg'
 
 export default function ContactForm() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [disabled, setDisabled] = useState(false)
+    const [emailSent, setEmailSent] = useState(null)
+
+
     return (<>
 
         <Container>
@@ -28,17 +38,17 @@ export default function ContactForm() {
                             <Form>
                                 <Form.Group controlId="exampleForm.ControlInput1">
                                     <Form.Label>Name</Form.Label>
-                                    <Form.Control type="email" placeholder="John Smith" />
+                                    <Form.Control type="name" placeholder="John Smith" value={name}  onChange={e=>setName(e.target.value)} />
                                 </Form.Group>
 
                                 <Form.Group controlId="exampleForm.ControlInput1">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="name@example.com" />
+                                    <Form.Control type="email" placeholder="name@example.com"  value={email} onChange={e=>setEmail(e.target.value)}/>
                                 </Form.Group>
                             </Form>
                             <Form.Group controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Message</Form.Label>
-                                <Form.Control as="textarea" rows="3" />
+                                <Form.Control as="textarea" rows="3"  onChange={()=>setMessage(message)} />
                             </Form.Group>
                             <Button variant="primary"  active>
                                 Send
@@ -51,7 +61,7 @@ export default function ContactForm() {
     <style jsx global>{`
     
     .jumbo{
-        background-image:url(${email});
+        background-image:url(${email1});
         background-size:cover;
         background-position:center;
         height:250px;
