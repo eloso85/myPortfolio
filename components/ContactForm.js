9a59a4323638338aec70e2c487b8aa2c89744978
Alejandro Segura <eloso85@gmail.com>
2020-05-22 21:13:25 -0500

    2nd try

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -23,107 +23,64 @@ export default function ContactForm() {
 
        //hello
 
-        setDisabled({
-            disabled:true
-            });
+        // setDisabled({
+        //     disabled:true
+        //     });
 
-        setEmailSent({
-            emailSent:false,
-
-        })
-      }
-     
-
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [disabled, setDisabled] = useState(false)
-    const [emailSent, setEmailSent] = useState(null)
-
-    const handleResponse = (status, msg) => {
-        if (status === 200) {
-          setDisabled({
-            disabled: true,
+        // setEmailSent({
+        //     emailSent:false,
+    
             
-            
-          })
-          setEmail({
-            email: ''
-            })
-          setMessage({
-              message:''
-          })  
-        } else {
-          setEmailSent({
-            info: { error: true, msg: msg }
-          })
-        }
-      }
-
-    const handleSubmit = async e => {
-        e.preventDefault()
-        setDisabled(prevStatus => ({ ...prevStatus, disabled: true }))
-        const res = await fetch('/api/send', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: 
-          JSON.stringify({
-              name:"",
-              email:"",
-              message:""
-          })
-        })
-        const text = await res.text()
-        handleResponse(res.status, text)
-      }
+        // })    
+        // alert("email not sent")
+        // console.log(evt)
+    }
 
 
     return (<>
 
+        <Container>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Header>
+                            <Jumbotron  className='jumbo' style={{ textAlign: 'center' }}>
 
 
-          <Container>
-              <Row>
-                  <Col>
-                      <Card>
-                          <Card.Header>
-                              <Jumbotron  className='jumbo' style={{ textAlign: 'center' }}>
+                                <h1 style={{fontWeight:'bold'}}>Contact Me</h1>
 
 
-                                  <h1 style={{fontWeight:'bold'}}>Contact Me</h1>
+                            </Jumbotron>
+                        </Card.Header>
+                        <Card.Body>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group controlId="exampleForm.ControlInput1">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control type="name" placeholder="John Smith" value={name}  onChange={e=>setName(e.target.value)} />
+                                </Form.Group>
 
-
-                              </Jumbotron>
-                          </Card.Header>
-                          <Card.Body>
-                              <Form onSubmit={handleOnSubmit}>
-                                  <Form.Group controlId="exampleForm.ControlInput1">
-                                      <Form.Label>Name</Form.Label>
-                                      <Form.Control type="name" placeholder="John Smith" value=''  onChange='' />
-                                  </Form.Group>
-
-                                  <Form.Group controlId="exampleForm.ControlInput1">
-                                      <Form.Label>Email address</Form.Label>
-                                      <Form.Control id="email" type="email" placeholder="name@example.com" required value={inputs.email} onChange={handleOnChange}/>
-                                  </Form.Group>
-                              
-                              <Form.Group controlId="exampleForm.ControlTextarea1">
-                                  <Form.Label>Message</Form.Label>
-                                  <Form.Control as="textarea" rows="3" id="message" required value ={inputs.message} onChange={handleOnChange} />
-                              </Form.Group>
-                              <Button variant="primary" type="submit" disabled= {status.submitting}>
-                                  Send
-                              </Button>
-
+                                <Form.Group controlId="exampleForm.ControlInput1">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="name@example.com"  value={email} onChange={e=>setEmail(e.target.value)}/>
+                                </Form.Group>
                             
-                          </Form>
-                          </Card.Body>
-                      </Card>
-                  </Col>
-              </Row>
-          </Container>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control as="textarea" rows="3"  value ={message} onChange={e=>setMessage(e.target.value)} />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" disabled= {disabled}>
+                                Send
+                            </Button>
+
+                            {emailSent === true && <p className="success-msg">Email Sent</p>}
+                            {emailSent === false && <p className="err-msg">Email Not Sent</p>}
+
+                        </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     <style jsx global>{`
     
     .jumbo{
