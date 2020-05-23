a467cfee3240c936ae62abd21edefb64cb6fc502
Alejandro <eloso85@gmail.com>
2020-05-22 20:40:13 -0500

    ughhhh

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -12,34 +12,141 @@ import email1 from '../src/images/email2.jpg'
 
 export default function ContactForm() {
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [disabled, setDisabled] = useState(false)
-    const [emailSent, setEmailSent] = useState(null)
-
-    const handleSubmit =(evt)=>{
-        evt.preventDefault();
-
-       
-
-        // setDisabled({
-        //     disabled:true
-        //     });
-
-        // setEmailSent({
-        //     emailSent:false,
+    const [status, setStatus] = useState({
+        submitted: false,
+        submitting: false,
+        info: { error: false, msg: null }
+      })
     
+      const [inputs, setInputs] = useState({
+        email: '',
+        message: ''
+      })
+    
+      const handleResponse = (status, msg) => {
+        if (status === 200) {
+          setStatus({
+            submitted: true,
+            submitting: false,
+            info: { error: false, msg: msg }
+          })
+          setInputs({
+            email: '',
+            message: ''
+          })
+        } else {
+          setStatus({
+            info: { error: true, msg: msg }
+          })
+        }
+      }
+    
+      const handleOnChange = e => {
+        e.persist()
+        setInputs(prev => ({
+          ...prev,
+          [e.target.id]: e.target.value
+        }))
+        setStatus({
+          submitted: false,
+          submitting: false,
+          info: { error: false, msg: null }
+        })
+      }
+    
+      const handleOnSubmit = async e => {
+        e.preventDefault()
+        setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
+        const res = await fetch('http://localhost:3000/api/send', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(inputs)
+        })
+        const text = await res.text()
+        handleResponse(res.status, text)
+      }
+
+    // const [name, setName] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [message, setMessage] = useState('')
+    // const [disabled, setDisabled] = useState(false)
+    // const [emailSent, setEmailSent] = useState(null)
+
+    // const handleResponse = (status, msg) => {
+    //     if (status === 200) {
+    //       setDisabled({
+    //         disabled: true,
             
-        // })    
-        // alert("email not sent")
-        // console.log(evt)
-    }
+            
+    //       })
+    //       setEmail({
+    //         email: ''
+    //         })
+    //       setMessage({
+    //           message:''
+    //       })  
+    //     } else {
+    //       setEmailSent({
+    //         info: { error: true, msg: msg }
+    //       })
+    //     }
+    //   }
+
+    // const handleSubmit = async e => {
+    //     e.preventDefault()
+    //     setDisabled(prevStatus => ({ ...prevStatus, disabled: true }))
+    //     const res = await fetch('/api/send', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json'
+    //       },
+    //       body: 
+    //       JSON.stringify({
+    //           name:"",
+    //           email:"",
+    //           message:""
+    //       })
+    //     })
+    //     const text = await res.text()
+    //     handleResponse(res.status, text)
+    //   }
 
 
     return (<>
 
-        <Container>
+<form onSubmit={handleOnSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          onChange={handleOnChange}
+          required
+          value={inputs.email}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          onChange={handleOnChange}
+          required
+          value={inputs.message}
+        />
+        <button type="submit" disabled={status.submitting}>
+          {!status.submitting
+            ? !status.submitted
+              ? 'Submit'
+              : 'Submitted'
+            : 'Submitting...'}
+        </button>
+      </form>
+      {status.info.error && (
+        <div className="error">Error: {status.info.msg}</div>
+      )}
+      {!status.info.error && status.info.msg && (
+        <div className="success">{status.info.msg}</div>)}
+
+        {/* <Container>
             <Row>
                 <Col>
                     <Card>
@@ -53,34 +160,32 @@ export default function ContactForm() {
                             </Jumbotron>
                         </Card.Header>
                         <Card.Body>
-                            <Form onSubmit={handleSubmit}>
+                            <Form onSubmit={handleOnSubmit}>
                                 <Form.Group controlId="exampleForm.ControlInput1">
                                     <Form.Label>Name</Form.Label>
-                                    <Form.Control type="name" placeholder="John Smith" value={name}  onChange={e=>setName(e.target.value)} />
+                                    <Form.Control type="name" placeholder="John Smith" value=''  onChange='' />
                                 </Form.Group>
 
                                 <Form.Group controlId="exampleForm.ControlInput1">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="name@example.com"  value={email} onChange={e=>setEmail(e.target.value)}/>
+                                    <Form.Control id="email" type="email" placeholder="name@example.com" required value={inputs.email} onChange={handleOnChange}/>
                                 </Form.Group>
                             
                             <Form.Group controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Message</Form.Label>
-                                <Form.Control as="textarea" rows="3"  value ={message} onChange={e=>setMessage(e.target.value)} />
+                                <Form.Control as="textarea" rows="3" id="message" required value ={inputs.message} onChange={handleOnChange} />
                             </Form.Group>
-                            <Button variant="primary" type="submit" disabled= {disabled}>
+                            <Button variant="primary" type="submit" disabled= {status.submitting}>
                                 Send
                             </Button>
 
-                            {emailSent === true && <p className="success-msg">Email Sent</p>}
-                            {emailSent === false && <p className="err-msg">Email Not Sent</p>}
-
+                           
                         </Form>
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
-        </Container>
+        </Container> */}
     <style jsx global>{`
     
     .jumbo{
--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
+require('dotenv').config()
 const withImages = require('next-images')
+
+module.exports = {
+    env: {
+      SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
+    }
+  }
+
+
 module.exports = withImages({})
+
